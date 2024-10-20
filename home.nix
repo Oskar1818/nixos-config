305f8163fd830e508ccb138a2fee48542e3bc598
oskar <oskar@apaq.se>
2024-10-20 22:15:39 +0200

    changed cursor size

--- home.nix
+++ home.nix
@@ -22,9 +22,10 @@
 
   home.pointerCursor = {
     gtk.enable = true;
+    # x11.enable = true;
     package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Ice";
-    size = 22;
+    name = "Bibata-Modern-Classic";
+    size = 16;
   };
 
   home.packages = with pkgs; [
